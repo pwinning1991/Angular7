3a782f85a769e3bf6d293dbb6716d4526d1bbc50
Philip Winnington <ftcpjw@yahoo.com>
2019-02-03 10:27:33 -0500

    added documents component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { DocumentsComponent } from './documents/documents.component';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
@@ -11,7 +12,8 @@ import { HomepageComponent } from './homepage/homepage.component';
   ],
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    DocumentsComponent
   ],
   bootstrap: [
     AppComponent
---
+++ src/app/documents/documents.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'documents',
+  templateUrl: 'documents.component.html'
+})
+
+export class DocumentsComponent {}
