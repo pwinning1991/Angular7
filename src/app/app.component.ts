89a0efff68b631edae784cdd44f3d06a0f350ea8
Philip Winnington <ftcpjw@yahoo.com>
2019-02-03 09:02:53 -0500

    updated file structure for homepage component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app',
+  template: '<h1>Navigation</h1>',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'freelance-app-fe';
+  title = 'Freelance Bootcamp Dashboard';
 }
