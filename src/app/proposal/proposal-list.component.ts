f795045068ee7a4c31f62a69e97b366e01922f90
Philip Winnington <ftcpjw@yahoo.com>
2019-02-03 14:31:31 -0500

    added proposal object contructor and exported class

--- src/app/proposal/proposal-list.component.ts
+++ src/app/proposal/proposal-list.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { Proposal } from './proposal';
 
 @Component({
   selector: 'proposal-list',
   templateUrl: 'proposal-list.component.html'
 })
 
-export class ProposalListComponent {}
+export class ProposalListComponent {
+  proposalOne: Proposal = new Proposal(15, 'abc company', 'http://dev.com', 'Ruby on Rails', 150, 120, 15,'pwinnington@pwinnington.com')
+  proposalTwo: Proposal = new Proposal(44, 'bb comapny', 'http://dev.com', 'React', 150, 120, 15,'pwinnington@pwinnington.com')
+  proposalThree: Proposal = new Proposal(99, 'lol company', 'http://dev.com', 'Angular', 150, 120, 15,'pwinnington@pwinnington.com')
+
+proposals: Proposal[] = [this.proposalOne, 
+this.proposalTwo, 
+this.proposalThree]
+}
