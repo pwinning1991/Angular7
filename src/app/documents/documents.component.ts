fe3a8d3e33ecd6bcf2e19f01a1922b8e8baee0b8
Philip Winnington <ftcpjw@yahoo.com>
2019-02-03 11:29:07 -0500

    updated to pull data into documetns view

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,8 +1,33 @@
 import { Component } from '@angular/core';
+import { Document } from './document';
 
 @Component({
   selector: 'documents',
   templateUrl: 'documents.component.html'
 })
 
-export class DocumentsComponent {}
+export class DocumentsComponent {
+  documents: Document[] = [
+    {
+      title: "My First Doc",
+      description: "asdfasdf asdfasdf",
+      file_url: "http://google.com",
+      updated_at: "11/11/16",
+      image_url: "http://google.com"
+    },
+    {
+      title: "My Second Doc",
+      description: "asdfasdf asdfasdf",
+      file_url: "http://google.com",
+      updated_at: "11/11/16",
+      image_url: "http://google.com"
+    },
+    {
+      title: "My Last  Doc",
+      description: "asdfasdf asdfasdf",
+      file_url: "http://google.com",
+      updated_at: "11/11/16",
+      image_url: "http://google.com"
+    }
+  ]
+}
